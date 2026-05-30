c9f71299f8654f2f7706600a6066b8ceb180778e
GAURI <gauriagarwal25@gmail.com>
2026-05-30 19:39:26 +0530

    Enable Nitro routing for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tsconfigPaths(), tailwindcss()],
+  plugins: [tanstackStart(), nitro(), react(), tsconfigPaths(), tailwindcss()],
 });
